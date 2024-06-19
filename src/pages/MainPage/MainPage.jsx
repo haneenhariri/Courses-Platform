f8c0c1e952fab4ed4620037c1c9733ca688d617d
olaEweer <olaeweer99@gmail.com>
2024-06-19 16:00:04 +0300

    add Component Hero

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,7 +1,9 @@
+import Hero from '../../components/Hero/Hero'
 import './MainPage.css'
 export default function MainPage() {
   return (
     <>
+      <Hero h1={"Explore thousands of hands-on creative classes."} p={'Start learning for '} span={'free'} button={'Choose a course'} discount={'50%'} days={'24 Days'} time={'17:44:16'} img={"./image/MainPageImg/HeroImg/Hero_1.png"} month={''} profession={''} courses={true} classImg1={'OE_main_image1'} classImg2={'OE_main_image2'} classVector={'OE_main_vector'}/>
     </>
   )
 }
