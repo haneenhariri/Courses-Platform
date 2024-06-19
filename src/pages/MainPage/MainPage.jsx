02a28090cab47545f973478a44e46bee710dc6cb
haneen <haneenhariry8@gmail.com>
2024-06-19 16:16:23 +0300

    HH section

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,11 +1,15 @@
 import NavBar from "../../components/NavBar/NavBar";
 import FOOTER from '../../components/Footer/Footer'
 import "./MainPage.css";
-
+import ChooseCourse from "../../components/ChooseCourse/ChooseCourse";
+import Acordeon from './../../components/Acordeon/Acordeon'
+import StudentSay from "../../components/StudentSay/StudentSay";
 export default function MainPage() {
   return (
     <>
       <NavBar />
+{/*       <ChooseCourse/> */}
+      <StudentSay/>
       <FOOTER />
     </>
   );
