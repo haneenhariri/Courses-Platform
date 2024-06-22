8e952c039511c095873dc28be33a2d1d07aa6df9
Ayamahmoud211 <aya.mahmoud.2111@gmail.com>
2024-06-22 15:53:56 -0400

    update

---
+++ src/components/Training/Training.jsx
@@ -0,0 +1,53 @@
+import './Training.css'
+export default function AMTrain() {
+    const AMList= [
+        { ID :0 ,
+          div : [{
+          id: '1.',
+          text1:'Study the material on the platform at any convenient time'
+          },
+          {id: '2.',
+          text1:'Do practical tasks, get feedback and consolidate knowledge'}]
+        },
+        { ID :1 ,
+          div : [{
+          id: '3.',
+          text1:'Prepare a project and add it to your portfolio'
+        },
+        {id: '4.',
+          text1:'Prepare a project and add it to your portfolio'}]
+        }
+  
+      ];
+    return (
+      <>
+       
+        <section className="AM-traning">
+          <h2>How is the training going</h2>
+  
+          
+          <div className="parentCard"> 
+            {AMList.map((ITem) => (
+              <div className="cards" key={ITem.ID}>
+                {ITem.div.map((item) => (
+                  <div key={item.id} className="card" >
+                    
+                    <h3 >{item.id}</h3>
+                    <div className="line">  </div>
+                    <p>{item.text1}</p>
+                    
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
+  
+        </section>
+        
+  
+        
+      </>
+    );
+  }
+  
+  
