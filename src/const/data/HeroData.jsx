1605815b55fa5cdc3838786105b812f85bd6f351
fatimah-mse <fatimahmse83@gmail.com>
2024-06-22 23:10:20 +0300

    edit-hero

--- src/const/data/HeroData.jsx
+++ src/const/data/HeroData.jsx
@@ -1,68 +1,53 @@
-
-export const MainData =[
-    {
-      title: 'Marketer',
-    id: '1',
-      class:"OE_main1"
-    },
-    {
-      title: 'Illustrator',
-      id: '2',
-       class:"OE_main2"
-    },
-    {
-      title: 'Animator',
-      id: '3',
-       class:"OE_main3"
-    },
-    {
-      title: 'Programmer',
-      id: '4',
-       class:"OE_main4"
-    },
-    {
-      title: 'Designer',
-      id: '5',
-       class:"OE_main5"
-    },
-    {
-      title: 'Photographer',
-      id: '6',
-       class:"OE_main6"
-    },
-    
+export const BtnsMainData = [
+  {
+      id: 1,
+      btnContent: 'Illustrator'
+  },
+  {
+      id: 2,
+      btnContent: 'Animator'
+  },
+  {
+      id: 3,
+      btnContent: 'Marketer'
+  },
+  {
+      id: 4,
+      btnContent: 'Programmer'
+  },
+  {
+      id: 5,
+      btnContent: 'Designer'
+  },
+  {
+      id: 6,
+      btnContent: 'Photographer'
+  }
 ]
 
-
-export const CoursesData =[
+export const BtnsCoursesData = [
   {
-      title: 'InVision',
-    id: '1',
-      class:"OE_courses1"
+      id: 1,
+      btnContent: 'ProtoPie'
   },
   {
-      title: 'ProtoPie',
-      id: '2',
-      class:"OE_courses2"
+      id: 2,
+      btnContent: 'Miro'
   },
   {
-      title: 'Miro',
-      id: '3',
-      class:"OE_courses3"
+      id: 3,
+      btnContent: 'InVision'
   },
   {
-      title: 'Photoshop',
-      id: '4',
-      class:"OE_courses4"
+      id: 4,
+      btnContent: 'Photoshop'
   },
   {
-    title: 'Figma',
-    id: '5',
-      class:"OE_courses5"
+      id: 5,
+      btnContent: 'Figma'
   },
   {
-    title: 'Illustrator',
-    id: '6',
-      class:"OE_courses6"
-}
+      id: 6,
+      btnContent: 'Illustrator'
+  }
 ]
