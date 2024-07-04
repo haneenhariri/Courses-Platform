1b9a0d4a4e79cfd8783a96fbe4ef0b79fad887b2
Ayamahmoud211 <aya.mahmoud.2111@gmail.com>
2024-07-04 12:52:47 -0400

    update

--- src/components/DashBoardComp/Form/Form.jsx
+++ src/components/DashBoardComp/Form/Form.jsx
@@ -1,11 +1,31 @@
+import { useState } from "react";
 import "./Form.css";
 import { Outlet } from "react-router-dom";
 
-const Form = ({ label1, label2, label3, label4, title, input1 , input2 , input3 , input4}) => {
+const Form = ({ label1, label2, label3, label4, title, input1 , input2 , input3 , input4 }) => {
+  
+  const [Data, setData] = useState({
+    text1:input1,
+    text2: input2,
+    text3: input3,
+    date: input4
 
+  });
+  const handelSave = (event) =>{
+    event.preventDefault();
+    
+
+  }
+  function handel(e) {
+    const newData = { ...Data }
+    newData[e.target]= e.target.value
+    setData(newData)
+    console.log(newData)
+    
+  }
   return (
     <section className="FM-Dash-Header">
-      <form className="BA-custom-form">
+      <form className="BA-custom-form" onSubmit={handelSave}>
 
       <h2>{title}</h2>
       
@@ -14,7 +34,8 @@ const Form = ({ label1, label2, label3, label4, title, input1 , input2 , input3 
           {label1} <span>*</span>
         </p>
         <input
-          value={input1}
+          onChange={(e) => handel(e)}
+          value={Data.text1}
           type="text"
         />
       </label>
@@ -23,7 +44,8 @@ const Form = ({ label1, label2, label3, label4, title, input1 , input2 , input3 
           {label2} <span>*</span>
         </p>
         <input
-          value={input2}
+          onChange={(e) => handel(e)}
+          value={Data.text2}
           type="text"
         />
       </label>
@@ -32,7 +54,8 @@ const Form = ({ label1, label2, label3, label4, title, input1 , input2 , input3 
           {label3} <span>*</span>
         </p>
         <textarea
-          value={input3}
+          onChange={(e) => handel(e)}
+          value={Data.text3}
         ></textarea>
       </label>
       <label>
@@ -40,7 +63,8 @@ const Form = ({ label1, label2, label3, label4, title, input1 , input2 , input3 
           {label4} <span>*</span>
         </p>
         <input
-          value={input4}
+          onChange={(e) => handel(e)}
+          value={Data.date}
           type="text"
         />
       </label>
